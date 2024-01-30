1078c059b19c8eb0b8111d89a9e6f72a90d8b72b
vi-the <vingo20@student.sdu.dk>
2024-01-30 02:21:26 +0100

    fixed pop out card and add to card button works now

--- GabiSushi/app.js
+++ GabiSushi/app.js
@@ -1,15 +1,15 @@
 const openShopping = document.querySelector(".shopping");
 const closeShopping = document.querySelector(".closeShopping");
 const list = document.querySelector(".list");
-const listCard = document.querySelector("listCard");
-const total = document.querySelector("total");
+const listCard = document.querySelector(".listCard");
+const total = document.querySelector(".total");
 const body = document.querySelector("body");
-const numProduct = document.querySelector("numProduct");
+const numProduct = document.querySelector(".numProduct");
 
 openShopping.addEventListener("click", () => {
     body.classList.add("active")
 })
-openShopping.addEventListener("click", () => {
+closeShopping.addEventListener("click", () => {
     body.classList.remove("active")
 })
 
@@ -53,18 +53,61 @@ let products = [
 ]
 
 let listCards = [];
-const initApp =() => {
+const initApp = () => {
     products.forEach((value,key) =>{
         let newDiv = document.createElement("div");
-        newDiv.classList.add("items");
+        newDiv.classList.add("item");
         newDiv.innerHTML =`
             <img src ="images/${value.images}">
             <div class ="title">${value.name}</div>
             <div class="price">${value.price.toLocaleString()}</div>
             <button onclick="addToCard(${key})">Add To Card</button>
-        `
+        `;
         list.appendChild(newDiv)
     })
 }
 
 initApp()
+
+const addToCard = key => {
+    if(listCards[key] == null){
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].numProduct = 1;
+        refreshCard();
+    } else {
+        listCards[key].numProduct++;
+        refreshCard(); 
+    }
+    
+}
+const refreshCard = () => {
+    listCard.innerHTML="";
+    let count = 0;
+    let totalPrice = 0;
+
+    listCards.forEach((value, key) => {
+        if (value != null) {
+            let itemTotalPrice = value.price * value.numProduct; // Calculate total price for this item
+            totalPrice += itemTotalPrice; // Add to the total price
+            count += value.numProduct; // Add to the total count
+
+            let newDiv = document.createElement("li");
+            newDiv.innerHTML = `
+                <div><img src ="images/${value.images}"></div>
+                <div class ="cardName">${value.name}</div>
+                <div class ="cardPrice">${itemTotalPrice.toLocaleString()}</div>
+
+                <div>
+                    <button style="background-color: orange" class="cardButton" onclick="changeNumProduct(${key}, ${value.numProduct - 1})">-</button>
+                    <div class ="count">${value.numProduct}</div>
+                    <button style="background-color: orange" class="cardButton" onclick="changeNumProduct(${key}, ${value.numProduct + 1})">+</button>
+                </div>
+            `;
+            listCard.appendChild(newDiv);
+        }
+
+        total.innerText = totalPrice.toLocaleString();
+        numProduct.innerText = count;
+    })
+    
+}
